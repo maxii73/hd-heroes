13ebf885a192b2d3d8658b1ef9dfcec58a90d8a7
maxii73 <athousandsuns73@gmail.com>
2022-06-17 11:13:37 -0300

    terminado posta

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.listen (port, () => console.log(`Server corriendo en el puerto: ${port}`));
 
 //Empiezo con las rutas
 app.get('/home',(req,res) => res.sendFile(path.join(__dirname, 'views/index.html')));
+app.get('/index',(req,res) => res.sendFile(path.join(__dirname, 'views/index.html')));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views/index.html')));
 app.get('/babbage',(req,res) => res.sendFile(path.join(__dirname, 'views/babbage.html')));
 app.get('/berners-lee',(req,res) => res.sendFile(path.join(__dirname, 'views/berners-lee.html')));
