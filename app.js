b017890cc18ef2cce148206c97ea5c3ae467552f
maxii73 <athousandsuns73@gmail.com>
2022-06-17 10:58:29 -0300

    tp terminado

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const path = require ('path');
 const app = express();                      //Lo ejecuto
 const port = 3030;
 
+app.use(express.static('public'));  //Aclaro donde estan mis archivos estaticos.
+
 //levanto mi server
 app.listen (port, () => console.log(`Server corriendo en el puerto: ${port}`));
 
 //Empiezo con las rutas
 app.get('/home',(req,res) => res.sendFile(path.join(__dirname, 'views/index.html')));
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views/index.html')));
 app.get('/babbage',(req,res) => res.sendFile(path.join(__dirname, 'views/babbage.html')));
 app.get('/berners-lee',(req,res) => res.sendFile(path.join(__dirname, 'views/berners-lee.html')));
 app.get('/clarke',(req,res) => res.sendFile(path.join(__dirname, 'views/clarke.html')))
